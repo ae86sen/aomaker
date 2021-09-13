79ca1842c1bed5efbb9c15bfd09682311c8eed3d
ancientone <listeningsss@163.com>
2021-09-13 11:01:24 +0800

    update v1.0.3

--- aomaker/__init__.py
+++ aomaker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __description__ = "help you play your API like building blocks !"
 __image__ = r"""
          /\                         .-.                 .:                      
