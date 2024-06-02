f746d9ddfab54aaec8c1de9d989486d655909f75
listen <listenzhang@yunify.com>
2024-06-02 22:12:14 +0800

    1.增加异步任务结果到目标接口响应中 2.修复BaseApi子类重写send_http报错问题

--- aomaker/__init__.py
+++ aomaker/__init__.py
@@ -1,6 +1,6 @@
 from emoji import emojize
 
-__version__ = "2.4.5"
+__version__ = "2.4.6"
 __description__ = "Quickly Arrange,Quickly Test!"
 __image__ = emojize(fr"""
               :----.                                                             ::::
